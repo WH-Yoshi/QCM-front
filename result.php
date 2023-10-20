30e08631c7ab4d68bc797f6a209cab232d062f86
WH-Yoshi <130357229+WH-Yoshi@users.noreply.github.com>
2023-10-20 22:37:08 +0200

    Onto the qcm

---
+++ result.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Luca Abs">
+    <title>Henallux QCM</title>
+    <link href="styles/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope&family=Montserrat&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <img src="images/logo.png" alt="Logo Henallux" >
+            <h3>QCM - Technologie WEB</h3>
+        </nav>
+    </header>
+    <main>
+        <section class="qcm-result">
+            
+        </section>       
+    </main>
+    <footer>
+        <img src="images/logo.png" alt="Logo Henallux" >
+    </footer>
+</body>
+</html>
