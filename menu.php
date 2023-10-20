e64f74d3e7ad9cf3cd698b9dfdf3cf04204bb3aa
WH-Yoshi <130357229+WH-Yoshi@users.noreply.github.com>
2023-10-20 22:00:07 +0200

    New menu

--- menu.php
+++ menu.php
@@ -27,45 +27,21 @@
             <h2>QCM : L'informatique de base</h2>
             <section id="panels" class="section-panels">
                 <article class="start">
-                    <h5>Informations</h5>
-                    <p>Cet examen se compose de 10 questions choisies au hasard sur les 20 que vous avez étudiés. Il n’y a qu’une seule reponse possible par question</p>
-                    <a href="target"><button>Commencer</button></a>
+                    <div>
+                        <h5>Informations</h5>
+                        <p>Cet examen se compose de 10 questions choisies au hasard sur les 20 que vous avez étudiés. Il n’y a qu’une seule reponse possible par question</p>
+                    </div>
+                    <a href="target" class="button">Commencer</a>
                 </article>
                 <article class="result">
-                    <h5>Reslutats</h5>
-                    <p>Pour verifier les resultat après avoir fini l’examen appuyer ici</p>
-                    <a href="target"><button>Resultats</button></a>
-                </article>
-                <!-- <div style="width: 1596px; height: 665px; position: relative">
-                    <div style="left: 514px; top: 0px; position: absolute; color: white; font-size: 40px; font-family: Inter; font-weight: 600; word-wrap: break-word">QCM : L’informatique de base </div>
-                    <div style="width: 626px; height: 495px; left: 970px; top: 170px; position: absolute">
-                        <div style="width: 145px; height: 39px; left: 240px; top: 51px; position: absolute; color: white; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Resultats</div>
-                        <div style="width: 626px; height: 495px; left: 0px; top: 0px; position: absolute; border: 3px black solid"></div>
-                        <div style="width: 304px; height: 92px; left: 161px; top: 341px; position: absolute; background: black; border-radius: 35px"></div>
-                        <div style="width: 114px; height: 39px; left: 256px; top: 367px; position: absolute; color: white; font-size: 32px; font-family: Inter; font-weight: 600; word-wrap: break-word">Vérifier</div>
-                        <div style="width: 560px; left: 33px; top: 132px; position: absolute; color: white; font-size: 20px; font-family: Inter; font-weight: 600; word-wrap: break-word">Pour verifier les resultat après avoir fini l’examen appuyer ici</div>
+                    <div>
+                        <h5>Resultats</h5>
+                        <p>Pour verifier les resultat après avoir fini l’examen appuyer ici</p>
                     </div>
-                    <div style="width: 626px; height: 495px; left: 0px; top: 170px; position: absolute">
-                        <div style="width: 304px; height: 92px; left: 161px; top: 341px; position: absolute; background: black; border-radius: 35px"></div>
-                        <div style="width: 626px; height: 495px; left: 0px; top: 0px; position: absolute; border: 3px black solid"></div>
-                    </div> -->
-                </div>                
+                    <a href="target" class="button">Resultats</a>
+                </article>              
             </section>
-        </section>
-
-
-        <!-- <section id="connexion" class="section-connexion">
-            <h2>Page de connexion</h2>
-            <form id="form" method="post" action="./scripts/login.php">
-                <label for="Identifiant">Identifiant</label>
-                <input type="text" name="Identifiant" id="Identifiant">
-                <label for="Motdepasse">Mot de passe</label>
-                <input type="password" name="Motdepasse" id="Motdepasse">
-                <input type="submit" value="Se connecter" class="cta">
-                <h4 id="error"></h4>
-            </form>
-        </section> -->
-        
+        </section>        
     </main>
     <footer>
         <img src="images/logo.png" alt="Logo Henallux" >
