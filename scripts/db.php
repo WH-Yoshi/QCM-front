7fa705f68ee107b4aa37b7cd3a6acda1401bc973
WH-Yoshi <130357229+WH-Yoshi@users.noreply.github.com>
2023-12-21 17:20:42 +0100

    c'est fini quoi

--- scripts/db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-try {
-    $db = new PDO('mysql:host=192.168.85.206;dbname=testQCM2', 'techweb', 'Tigrou007=');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error: ". $e->getMessage());
-}
-return $db;
--- scripts/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require('examcheck.php');
-session_unset();
-session_destroy();
-echo json_encode(['message' => 'Déconnexion réussie']);
-header("Location: ../connection.php");
-exit();
